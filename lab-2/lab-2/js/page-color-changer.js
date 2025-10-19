14c13fe2d17d4101e0f9458b261f302e2c576d29
Ritika9815 <ritika9815@example.com>
2025-10-18 20:10:43 -0400

    Declare constants for red, green, and blue sliders and HTML element

---
+++ lab-2/lab-2/js/page-color-changer.js
@@ -0,0 +1,12 @@
+/* ****************************************************
+   File: page-color-changer.js
+   Description: This script changes the HTML background color
+                based on three range sliders for Red, Green, and Blue
+**************************************************** */
+
+/* Declare and initialize constants for the slider inputs */
+const redSlider = document.getElementById("red");
+const greenSlider = document.getElementById("green");
+const blueSlider = document.getElementById("blue");
+
+
